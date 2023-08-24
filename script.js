67cb3a618cb27645e3527be1661726387b8b9e54
Kevin Mora <kambetacode@gmail.com>
2023-08-24 17:56:27 -0500

    Added function to create numpad, created new results div, added styling to the numpad

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
-const numPad = document.getElementById('num-pad')
+const numPad = document.getElementById('num-pad');
+let AC;
+let result;
+
+const buttonTexts = [
+    'AC',
+    '+/-',
+    '%',
+    '÷',
+    '7',
+    '8',
+    '9',
+    'X',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '↶',
+    '0',
+    '.',
+    '=',
+];
 
 function createGrid() {
+    let buttonIndex = 0
     for (let row = 0; row < 4; row++) {
         for (let col = 0; col < 5; col++) {
             let num = document.createElement('div');
+            let button = document.createElement('button')
+            button.innerHTML = buttonTexts[buttonIndex];
+
+            button.className = 'buttons'
             num.className = 'gridNum'; 
-            num.setAttribute('id', `cell-${row}-${col}`)
+            num.setAttribute('id', `${buttonTexts[buttonIndex]}`)
             numPad.appendChild(num);
+            num.appendChild(button)
+
+            buttonIndex++
         }
+
+
     }
+    events()
+
+
 }
 
-createGrid()
+function events() {
+    AC = document.querySelector('#AC')
+    
+    AC.addEventListener('click', () => console.log('TEST'))
+}
+
+
+
+createGrid() 
+
+// ------------------------------------------------------------------------------------------------------------------------------------------

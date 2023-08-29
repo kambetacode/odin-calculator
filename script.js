1494ee209526d9cb1f2a06c100113e1b08169673
Kevin Mora <kambetacode@gmail.com>
2023-08-29 17:14:46 -0500

    Added styles to make it look nice, added functionality to add decimal numbers

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const numPad = document.getElementById('num-pad');
-const results = document.getElementById('results')
+const results = document.getElementById('results');
+const resultsOne = document.getElementById('results-one');
+const resultsTwo = document.getElementById('results-two');
 
 
+let signCheck = false
 let num1 = [];
-let num1Display;
-let num2;
+let num2 = [];
+let num1Display = 0;
+let num2Display = 0;
+let sign = ''
+
+
+let result = 0;
+let finalResult;
 let buttons;
 let AC;
-let result = 0;
 let parenthesis;
 let remaining;
 let two;
@@ -97,73 +105,203 @@ function events() {
     equal = document.querySelector('#a34')
     buttons = document.querySelectorAll('.buttons')
 
-    buttons.forEach(item => {
-        item.addEventListener('mouseover', (e) => {
-            console.log(e)
-            e.target.classList.add('hovered')
-            
-        })
+
+// --------------------------------Operators logic----------------------------------------------------------------------------------------
+
+    sumSign.addEventListener('click', () => {
+        if(num1Display === 0) {
+            alert('invalid format')
+        } else {
+            if(signCheck === false) {
+            signCheck = true
+            sign = '+'
+            resultsOne.innerHTML = `${num1Display} ${sign}`
+            }
+        }
     })
 
+    minusSign.addEventListener('click', () => {
+        if(num1Display === 0) {
+            alert('invalid format')
+        }   else {
+            if(signCheck === false) {
+            signCheck = true
+            sign = '-'
+            resultsOne.innerHTML = `${num1Display} ${sign}`
+            }
+        }
+    })
+
+    multiplySign.addEventListener('click', () => {
+        if(num1Display === 0) {
+            alert('invalid format')
+        }   else {
+            if(signCheck === false) {
+            signCheck = true
+            sign = '*'
+            resultsOne.innerHTML = `${num1Display} ${sign}`
+            }
+        }
+    })
+
+    divideSign.addEventListener('click', () => {
+        if(num1Display === 0) {
+            alert('invalid format')
+        }   else {
+            if(signCheck === false) {
+            signCheck = true
+            sign = '÷'
+            resultsOne.innerHTML = `${num1Display} ${sign}`
+            }
+        }
+    })
+
+//---------------------------------------------------------------------------------------------------------------------------------
+ 
+//---------------------------------Each button logic-------------------------------------------------------------------------------
     AC.addEventListener('click', () => {
         num1 = []
         num1Display = 0
-        results.innerHTML = `${num1Display}`
+        num2 = []
+        num2Display = 0
+        resultsOne.innerHTML = `${num1Display}`
+        resultsTwo.innerHTML = ''
+        signCheck = false
     })
 
     one.addEventListener('click', () => {
-        addNumberOne(1)
-        results.innerHTML = `${num1Display}`
+        if(signCheck === false) {
+            addNumberOne(1)
+            resultsOne.innerHTML = `${num1Display}`
+
+        } else {
+            addNumberTwo(1)
+            resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+        }
     })
 
     two.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(2)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(2)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     three.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(3)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(3)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     four.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(4)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(4)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     five.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(5)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(5)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     six.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(6)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(6)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     seven.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(7)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(7)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     eight.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(8)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(8)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     nine.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(9)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(9)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     zero.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne(0)
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo(0)
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
     })
 
     dot.addEventListener('click', () => {
+        if(signCheck === false) {
         addNumberOne('.')
-        results.innerHTML = `${num1Display}`
+        resultsOne.innerHTML = `${num1Display}`
+    } else {
+        addNumberTwo('.')
+        resultsOne.innerHTML = `${num1Display} ${sign} ${num2Display}`
+    }
+    })
+
+    equal.addEventListener('click', () => {
+        if(signCheck === false) {
+        resultsTwo.innerHTML = `${num1Display}`
+        resultsTwo.style.fontSize = '40px'
+    } else {
+        switch(sign) {
+            case '+': 
+            resultsTwo.innerHTML = `${add(num1Display, num2Display)}`
+            break
+            case '-': 
+            resultsTwo.innerHTML = `${subtract(num1Display, num2Display)}`
+            break
+            case '*': 
+            resultsTwo.innerHTML = `${multiply(num1Display, num2Display)}`
+            break
+            case '÷': 
+            resultsTwo.innerHTML = `${divide(num1Display, num2Display)}`
+            break
+        }
+        
+        
+    }
     })
 
     
@@ -174,27 +312,49 @@ function events() {
 
 createGrid() 
 
-results.innerHTML = `${result}`
+resultsOne.innerHTML = `${result}`
+resultsTwo.style.fontSize = '40px'
 
 // ------------------------------------------------------------------------------------------------------------------------------------------
 
+
+//----------------------------Functionality to add numbers and functions to make math calculations-------------------------------------------
+
 function addNumberOne(number) {
     num1.push(number)
-    num1Display = num1.join('')
+    if(num1[num1.length - 1] === '.') {
+        num1Display = num1.join('')
+    } else {
+    num1Display = parseFloat(num1.join(''))
+
+}
+}
+
+function addNumberTwo(number) {
+    num2.push(number)
+    if(num2[num2.length - 1] === '.') {
+        num2Display = num2.join('')
+    } else {
+    num2Display = parseFloat(num2.join(''))
+    }
 }
 
 function add(num1, num2) {
-    result = num1 + num2
+    finalResult = num1 + num2
+    return finalResult
 }
 
 function subtract(num1, num2) {
-    result = num1 - num2
+    finalResult = num1 - num2
+    return finalResult
 }
 
 function multiply(num1, num2) {
-    result = num1 * num2
+    finalResult = num1 * num2
+    return finalResult
 }
 
 function divide(num1, num2) {
-    result = num1 / num2
+    finalResult = num1 / num2
+    return finalResult
 }
